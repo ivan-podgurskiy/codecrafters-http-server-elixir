d313297e682bb8c05bbc595b812464a9db57e8c4
Ivan Podgurskiy <ivan.podgurskiy@gmail.com>
2026-07-13 22:05:20 +0300

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -11,10 +11,10 @@ defmodule Server do
 
     # TODO: Uncomment the code below to pass the first stage
     #
-    # # Since the tester restarts your program quite often, setting SO_REUSEADDR
-    # # ensures that we don't run into 'Address already in use' errors
-    # {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
-    # {:ok, _client} = :gen_tcp.accept(socket)
+    # Since the tester restarts your program quite often, setting SO_REUSEADDR
+    # ensures that we don't run into 'Address already in use' errors
+    {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
+    {:ok, _client} = :gen_tcp.accept(socket)
   end
 
   def main(_args) do
